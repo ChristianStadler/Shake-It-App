957180f022e5a5763928883dc2af303345cc87ee
SysHannah <sun_ann@web.de>
2014-03-07 16:03:45 +0100

    erste Screens für das UI

--- gen/de/dhbw/shake_it_app/R.java
+++ gen/de/dhbw/shake_it_app/R.java
@@ -8,6 +8,10 @@
 package de.dhbw.shake_it_app;
 
 public final class R {
+    public static final class array {
+        public static final int menus=0x7f060000;
+        public static final int stadtteil_arrays=0x7f060001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,21 +25,60 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_search=0x7f020000;
+        public static final int button_weiter=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Loginbutton=0x7f090003;
+        public static final int PWvergessenButton=0x7f090009;
+        public static final int RegistrierenButton=0x7f090008;
+        public static final int SeekBarAktShake=0x7f090012;
+        public static final int TextViewPasswort=0x7f090005;
+        public static final int action_settings=0x7f09001b;
+        public static final int content_frame=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int drawer_list=0x7f090002;
+        public static final int editTextName=0x7f09000d;
+        public static final int imageButtonWeiterClub=0x7f090018;
+        public static final int password=0x7f090004;
+        public static final int quickContactBadge1=0x7f09001a;
+        public static final int seekBarDurchschnShake=0x7f090013;
+        public static final int spinnerStadtteil=0x7f09000e;
+        public static final int textViewAktShake=0x7f090011;
+        public static final int textViewClub=0x7f090017;
+        public static final int textViewClubShakeIndize=0x7f090016;
+        public static final int textViewClubs=0x7f090015;
+        public static final int textViewDurchschnShake=0x7f090010;
+        public static final int textViewFilter=0x7f09000a;
+        public static final int textViewName=0x7f09000c;
+        public static final int textViewPosition=0x7f090019;
+        public static final int textViewSpitzname=0x7f090006;
+        public static final int textViewStadtteil=0x7f09000f;
+        public static final int username=0x7f090007;
+        public static final int viewblue=0x7f09000b;
+        public static final int viewblueClubs=0x7f090014;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int main_screen=0x7f030003;
+        public static final int profil=0x7f030004;
+        public static final int rangliste=0x7f030005;
+        public static final int registrierung=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050004;
+        public static final int drawer_open=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int stadtteil_prompt=0x7f050005;
     }
     public static final class style {
         /** 
@@ -59,10 +102,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
