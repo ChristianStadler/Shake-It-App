3f12b16ec5d2107eba4b473d227902abbca8beec
Christian <Christian@192.168.11.3>
2014-03-19 23:55:48 +0100

    - Change the AndroidManifest to add some connection permissions - Implement a raw ModelController - Add some documents for connection

--- src/de/dhbw/shake_it_app/model/ModelController.java
+++ src/de/dhbw/shake_it_app/model/ModelController.java
@@ -1,11 +1,63 @@
 package de.dhbw.shake_it_app.model;
 
-import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ModelController {
+
+	public ModelController() throws Exception {
+//		Gson gson = new Gson();
+		getLocation();
+	}
 	
-	public ModelController() {
-		Gson gson = new Gson();
+	public boolean getConnectivityState() {
+//		ConnectivityManager con = (ConnectivityManager).getSystemService(Context.CONNECTIVITY_SERVICE);
+//		NetworkInfo networkInfo = con.getActiveNetworkInfo();
+//		if (networkInfo != null && networkInfo.isConnected()) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+		return true;
+	}
+	
+	public String getLocation() throws Exception {
+		InputStream is = null;
+		try {
+			URL url = new URL("http://space-labs.appspot.com/repo/2855006/shakeit/api/location.sjs");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				
+			conn.setRequestMethod("GET");
+			conn.connect();
+				
+			int response = conn.getResponseCode();
+			is = conn.getInputStream();
+				
+			// Convert the InputStream into a string
+			String contentAsString = convertStreamToString (is); //Nächste Folie
+			
+			return contentAsString;
+		} finally {
+			if(is != null) {
+				is.close();
+			}		
+		}
+	}
+	
+	public static String convertStreamToString(InputStream is) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		
+		is.close();
+		return sb.toString();
 	}
 
 }
