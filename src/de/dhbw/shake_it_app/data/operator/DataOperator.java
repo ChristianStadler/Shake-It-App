f9d9b301bf25aef4b777305a41dbc891766f5f5b
Arne <Arne@141.72.129.163>
2014-04-02 09:48:51 +0200

    returnOverallClubIndex implementiert returnOverallUserIndex implementiert

--- src/de/dhbw/shake_it_app/data/operator/DataOperator.java
+++ src/de/dhbw/shake_it_app/data/operator/DataOperator.java
@@ -2,6 +2,7 @@ package de.dhbw.shake_it_app.data.operator;
 
 import de.dhbw.shake_it_app.data.DataProvider;
 import de.dhbw.shake_it_app.data.model.Session;
+import de.dhbw.shake_it_app.data.model.User;
 
 public class DataOperator {
 	
@@ -16,6 +17,33 @@ public class DataOperator {
 			}
 		}
 		return (int) Math.round(cumulatedValues/amountValues);
+	}
+	
+	public int returnOverallLocationIndex(int clubID){
+		double cumulatedValues = 0;
+		int amountValues = 0;
+		Session[] sessions = (Session[]) DataProvider.get().getModel(DataProvider.Session, "filter=locationID&value="+clubID);
+		for(int i=0; i<sessions.length;i++){
+			if(!sessions[i].getIsActive()){
+				cumulatedValues+=sessions[i].getCurrentShakeIndex();
+				amountValues++;
+			}
+		}
+		return (int) Math.round(cumulatedValues/amountValues);
+	}
+	
+	public int returnOverallUserIndex(int userID){
+		double cumulatedValues = 0;
+		int amountValues = 0;
+		Session[] sessions = (Session[]) DataProvider.get().getModel(DataProvider.Session, "filter=userID&value="+userID);
+		for(int i=0; i<sessions.length;i++){
+			if(!sessions[i].getIsActive()){
+				cumulatedValues+=sessions[i].getCurrentShakeIndex();
+				amountValues++;
+			}
+		}
+		return (int) Math.round(cumulatedValues/amountValues);
+		
 	}
 
 }
